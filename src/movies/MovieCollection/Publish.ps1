340a837b9240e142a72541dd4006842a23459ee6
wernervn <wernervn@axxess.co.za>
2023-03-17 14:39:47 +0200

    set global version per app type e.g. movie or series

---
+++ src/movies/MovieCollection/Publish.ps1
@@ -0,0 +1,6 @@
+﻿$app_version = Get-Content '../AppVersion.txt'
+$version = $app_version[0]
+$project = './MovieCollection.csproj'
+
+dotnet build $project -c Release -p:Version=$version
+dotnet publish $project -c Release --no-build --output C:/Publish/MovieCollection -p:Version=$version --nologo
